57e1eddd3f1b5a53c2a5e8e4ca2c108da3c01c14
tnmv <99592291+tnmv@users.noreply.github.com>
2023-09-14 10:35:09 +0100

    Update inventory.sh

--- inventory.sh
+++ inventory.sh
@@ -4,5 +4,4 @@ export cp01=$(cat /home/ubuntu/cp01)
 export cp02=$(cat /home/ubuntu/cp02)
 export cp03=$(cat /home/ubuntu/cp03)
 echo -e  "[controlplane] \ncp01 ansible_host=$cp01 ansible_ssh_private_key_file=ssh_key.pem \ncp02 ansible_host=$cp02 ansible_ssh_private_key_file=ssh_key.pem \ncp03 ansible_host=$cp03 ansible_ssh_private_key_file=ssh_key.pem \n[worker] \n \n[controlplane:vars] \nnode_role=master" > inventory
-chmod +x inventory.sh
-./inventory.sh
+
